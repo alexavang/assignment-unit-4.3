5f323379dbd12a968848fd66d65ec2ce0f62e1fe
Alex Vang <VangAlexa25@gmail.com>
2024-03-17 09:39:29 -0500

    second commit

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,10 +3,15 @@ console.log("***** Cart Functions *****");
 // We want to see how you are testing your code!!!
 
 let basket = [];
+const maxItems = 5;
 
 function addItem(item) {
-  basket.push(item);
-  return true;
+  if (isFull()) {
+    return false;
+  } else {
+    basket.push(item);
+    return true;
+  }
 }
 
 function listItems() {
@@ -19,14 +24,32 @@ function empty() {
   basket.length = 0;
 }
 
+function isFull() {
+  if (basket.length < maxItems) {
+    return false;
+  } else if (basket.length >= maxItems) {
+    return true;
+  }
+}
+
+function removeItem(item) {
+  if (basket.includes(item)) {
+      const i = basket.indexOf(item);
+      basket.splice(i, 1);
+      return item;
+  } else {
+      return null;
+  }
+}
+
 // Testing
 
 console.log("Adding Items:");
 addItem("Coffee");
 addItem("Tea");
 addItem("Water");
+addItem("Apple Juice");
 addItem("Energy Drink");
-addItem("Soju");
 console.log(basket);
 
 console.log("List of Items:");
@@ -39,6 +62,19 @@ console.log(basket);
 
 console.log("List of Items (After Emptying):");
 listItems();
+console.log(basket);
+
+console.log(basket.length);
+console.log(maxItems);
+console.log(isFull());
+
+console.log("List of Items (Adding):");
+addItem("Soju");
+console.log(basket);
+console.log(basket.length);
+
+console.log("List of Items (Removing):");
+removeItem("Soju")
 console.log(basket);
 
 // DO NOT MODIFY
